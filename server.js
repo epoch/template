1ecbe9a76bb97c92a6ed9b603d447da9c273d25e
DT <tsui.daniel@gmail.com>
2014-05-20 15:06:24 +1000

    added less middleware support

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var port = process.env.PORT || 3001;
 var express = require('express');
+var lessMiddleware = require('less-middleware');
 var path = require('path');
 var game = require('./app/app');
 
 var app = express();
-app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 var server = app.listen(port, function() {
-  console.log('Magic happens on port ' + port);
+  console.log('Magic happens on port %d', port);
 });
 
 var io = require('socket.io').listen(server);
